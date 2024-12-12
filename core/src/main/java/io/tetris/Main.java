186389bce198a912b6035c1dd78f0dda8b7b0132
JonV <jon.vazquezfernandez@elorrieta-errekamari.com>
2024-12-12 10:45:15 +0100

    background/table/score textures

--- core/src/main/java/io/tetris/Main.java
+++ core/src/main/java/io/tetris/Main.java
@@ -4,31 +4,59 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture background;
+    private Texture title;
+    private Texture table;
+    private Texture score;
+    
+   
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        title = new Texture("tetrisTitle.png");
+        table = new Texture("tabla.jpg");
+        score = new Texture("score.png");
+        background = new Texture("background.jpg");
+
+    
+
+
+       
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        final int width = Gdx.graphics.getWidth();
+        final int height = Gdx.graphics.getHeight();
+        batch.draw(background, 0, 0, width, height);
+        batch.draw(table, 40, 75, width - 240, height - 250);
+        batch.draw(title, 40, height - 160, width - 240, 150);
+        batch.draw(score, width - 180, height - 320, 150, 150);
         batch.end();
+        
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        title.dispose();
+        table.dispose();
+        score.dispose();
+        background.dispose();
     }
 }
